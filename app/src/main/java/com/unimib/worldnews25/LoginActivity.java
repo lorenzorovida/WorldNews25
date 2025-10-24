77ea5cd2218dadc45b5e6be0f103375670382766
narger <52449688+narger-ef@users.noreply.github.com>
2025-10-24 14:22:32 +0200

    Fine lezione

---
+++ app/src/main/java/com/unimib/worldnews25/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.unimib.worldnews25;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+
+        Button button = findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w(TAG, "Il bottone è stato cliccato");
+            }
+        });
+
+
+
+    }
+}
